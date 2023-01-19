5340a195f2ec85cfc0c3ec403c2562dc83e9c356
Anar Mamedov <anarmamedov.job@gmail.com>
2023-01-19 07:00:51 +0400

    deleting unnecessary comments.

--- src/encrypt_decrypt_from_file/Main.java
+++ src/encrypt_decrypt_from_file/Main.java
@@ -8,20 +8,12 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class Main extends HelpReading {
-    public static void main(String[] args) {
-//        readFileAndEncrypt(Path.of("Moriarty_letter_to_Sherlock.txt"), 6);
-//        System.out.println("*".repeat(15));
-//
-//        readFileAndDecrypt(Path.of("encrypted_with_key_6.txt"), 6);
-    }
 
     public static String readFileAndEncrypt(Path path, int key) {
-//        System.out.println(Encryption.encrypt(helpRead(path), key));
         return Encryption.encrypt(helpRead(path), key);
     }
 
     public static String readFileAndDecrypt(Path path, int key) {
-//        System.out.println(Decryption.decrypt(helpRead(path), key));
         return Decryption.decrypt(helpRead(path), key);
     }
 }
